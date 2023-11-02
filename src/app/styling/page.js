6861334938de41628e1d50ab71150d48bdd475cb
Aliyan Amir <tboy02722@gmail.com>
2023-11-03 01:59:09 +0500

    Next Js Practice

--- src/app/styling/page.js
+++ src/app/styling/page.js
@@ -3,10 +3,10 @@ import React, { useState } from 'react'
 import './style.css'
 
 export default function page() {
-const [h3style, seth3style] = useState({ backgroundColor: "yellow" })
-return (
-    <div>
-        {/* <h1 style={{backgroundColor:'yellow',color:'DarkOrange'}}>Styling in Next Js</h1> //// Inline Css ////// */}
+    const [h3style, seth3style] = useState({ backgroundColor: "yellow" })
+    return (
+        <div>
+            {/* <h1 style={{backgroundColor:'yellow',color:'DarkOrange'}}>Styling in Next Js</h1> //// Inline Css ////// */}
             <h3 style={h3style}>Styling in Next Js</h3>
             {/* <button onClick={() => seth3style({ backgroundColor: "lightblue" })} >Change Color</button> /// Update Style with UseState /////// */}
         </div>
