cd3a5fddb8bd6fae8729e6aa3c9a86a106eb0562
Aliyan Amir <tboy02722@gmail.com>
2023-11-04 02:12:16 +0500

    Next-Js Practice

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,22 @@
 'use client'  ///   Jab koi event call karta hai isko add karta hai sabsa top pa    ///
 import { useState } from 'react'
-import styles from './page.module.css'
+// import styles from './page.module.css'
 import Link from 'next/link'
+import outside from '@/app/styles/outside.module.css'
+import other from '@/app/styles/other.module.css'
+import style from './style.module.css'
 import { useRouter } from 'next/navigation'
+import Image from 'next/image'
+import vercimg from '../../public/vercel.svg'
+
+
 
 export default function Home() {
   const [name, setName] = useState("Aliyan")
+  const [color, setColor] = useState("green")
+  const { red } = style;
+  const { green } = style;
+
 
   const apple = (item) => {
     // alert(item)
@@ -29,7 +40,26 @@ export default function Home() {
 
       <button onClick={() => Navigate("/collection")}>Go to Collection Page</button><br /><br />
       <button onClick={() => Navigate("/contact")}>Go to Contact Page</button>
+      {/* <h1 className={outside.main}>This is a Module Css</h1>
+      <h1 id={other.main}>This is a Module Css 2</h1> */}
+      {/* <h1 className={color=='red'?style.red:style.green}>This is a Conditional Css</h1>
+      <h1 style={{backgroundColor:color=='red'?'red':'green'}}>This is a Conditional Css</h1> */}
+      {/* <h1 className={red}>This is a Text</h1>
+      <h1 className={green}>This is a Text</h1> */}
+
+      {/* /////////////// Image Optimization /////////// */}
+      <h1>Image Optimization in Next Js</h1>
+      
+      {/* <Image src={vercimg} /> */}
+      {/* <img  src={vercimg.src}/> */}
+      <Image src='https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png'
+        width={800}
+        height={800}
+      />
+
+
     </main>
+
   )
 }
 
